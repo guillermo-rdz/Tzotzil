22e45b6c5cae0fdf92a50b2d85d1c68cad6655fe
guillermo-rdz <nemesis_rmgj@hotmail.com>
2015-12-05 11:25:54 -0600

    cambio a la base de datos

--- app/controller/controller.php
+++ app/controller/controller.php
@@ -5,7 +5,11 @@
 	class controller{
 		// Views
 		public function home(){
-			include("app/vista/xxxx.html");
+			include("app/views/XXX.html");
+		}
+
+		public function welcome(){
+			include("app/views/welcome.html");	
 		}
 		//vista para lo errores u horrores
 		public function error(){
