8991161e33cb5c35ddf191c255793c55315efdc1
davymtz <davymtz91@gmail.com>
2015-12-11 00:12:24 -0600

    probando vistas y complementos

--- app/controller/controller.php
+++ app/controller/controller.php
@@ -11,11 +11,20 @@
 			include("app/view/login.html");
 		}
 		public function content(){
-			include("app/view/contenido.html");
+			session_start();
+			echo $_SESSION["area"];
+			//if (isset($_SESSION["conectado"])) {
+				include("app/view/contenido.html");
+			//} else
+			//	header('Location: /tzotzil/');
 		}
 		//vista para lo errores u horrores
 		public function error(){
-			include("app/view/404.shtml");
+			session_start();
+			//if (isset($_SESSION["conectado"])) {
+				include("app/view/404.shtml");
+			//} else
+			//	header('Location: /tzotzil/');
 		}
 	}
  ?>
--- app/view/js/funciones.js
+++ app/view/js/funciones.js
@@ -1,11 +1,14 @@
 (function (){
-	//$(document).on("ready", function (){
-		$.ajax({
-			type: "POST",
-			url: "app/model/model.php",
-			data: {"tipo":"preguntas_p","id_area":2}
-		}).done(function (info){
-			$(".contenido").html(info);
-		});
-	//});
+	var name_area = '<?php echo $_SESSION[\"area\"] ?>';
+	alert(name_area);
+
+	/*
+	$.ajax({
+		type: "POST",
+		url: "app/model/model.php",
+		data: {"tipo":"preguntas_p","id_area":2}
+	}).done(function (info){
+		$(".preguntas_p").html(info);
+	});
+	*/
 })();
