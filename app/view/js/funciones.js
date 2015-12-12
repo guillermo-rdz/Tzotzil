473def7afb0b82429f304551c7b39b6dff767d47
davymtz <davymtz91@gmail.com>
2015-12-11 19:38:20 -0600

    commit estupido

--- app/view/js/funciones.js
+++ app/view/js/funciones.js
@@ -1,9 +1,44 @@
 (function (){
 	$(".titulo__contenido").html(sesion_area);
-	alert("Su area es de tipo: "+sesion_area);
 	/*
+	alert("Su area es de tipo: "+sesion_area);
 	alert("Su usuario es de tipo: "+sesion_rango);
+	$("a[data='link1']").on("click",function (event){
+		event.preventDefault();
+		var valor = $("a[data='link1']").attr("href");
+		oculta(valor);
+	});
+	$("a[data='link2']").on("click",function (event){
+		event.preventDefault();
+		var valor = $("a[data='link2']").attr("href");
+		oculta(valor);
+	});
+	$("a[data='link3']").on("click",function (event){
+		event.preventDefault();
+		var valor = $("a[data='link2']").attr("href");
+		oculta(valor);
+	});
+
+	function oculta(elemento){
+		string={};
+		string.item1 = "#item1";
+		string.item2 = "#item2";
+		string.item3 = "#item3";
+		
+		var item=$(elemento);
+		if($(item).hasClass('visible')){
+			$(item).removeClass('visible');
+			$(item).addClass('invisible');
+			$(item).slideUp('fast');
+		} else {
+			$(item).removeClass('invisible');
+			$(item).addClass('visible');
+			$(item).slideDown('fast');
+		}
+	}
 	*/
+
+
 	$.ajax({
 		type: "POST",
 		url: "app/model/model.php",
@@ -11,6 +46,7 @@
 	}).done(function (info){
 		$(".preguntas_p").html(info);
 	});
+
 	$(".btnlogout").on("click", function (event){
 		event.preventDefault();
 		$.ajax({
